e73bea3c048ff6c876fa9d6e81b937306325c2a1
Matthias Balke <Mail@Matthias-Balke.de>
2011-09-04 11:38:36 +0200

    added js files to magento structure

---
+++ skin/frontend/base/balketechnologies/storemaintenance/js/timer.js
@@ -0,0 +1,63 @@
+
+// year, month, day, hours, minutes, seconds, milliseconds
+var end = new Date(2011, 08, 04, 13, 50, 00);          
+var daysElement    = null;
+var hoursElement   = null;
+var minutesElement = null;
+var secondsElement = null;
+
+timerId = setInterval("updateTimer(timerId)",1000);
+
+function getContainer(){
+    daysElement    = $('days');
+    hoursElement   = $('hours');
+    minutesElement = $('minutes');
+    secondsElement = $('seconds');
+}
+
+function updateTimer(timerId) {
+
+    var now = new Date();
+    var difference = end - now;
+
+
+
+    if (difference < 0){
+        if (null != daysElement)
+            daysElement.update("0");
+
+        if (null != hoursElement)
+            hoursElement.update("0");
+
+        if (null != minutesElement)
+            minutesElement.update("0");
+
+        if (null != secondsElement)
+            secondsElement.update("0");
+
+        clearInterval(timerId);
+        exit();
+    }
+
+    days = Math.floor(difference / 86400000);
+    difference = difference % 86400000;
+    hours = Math.floor(difference / 3600000);
+    difference = difference % 3600000;
+    minutes = Math.floor(difference / 60000);
+    difference = difference % 60000;
+    seconds = Math.floor(difference / 1000);
+    difference = difference % 1000;
+    msecs = difference;
+
+    if (null != daysElement)
+        daysElement.update(days);
+
+    if (null != hoursElement)
+        hoursElement.update(hours);
+
+    if (null != minutesElement)
+        minutesElement.update(minutes);
+
+    if (null != secondsElement)
+        secondsElement.update(seconds);
+}
